6b98b016d11fcd5629ca5d918cb1069f8bf5dc58
Cameron Hu <chu@profluent.bio>
2024-11-20 21:44:25 +0000

    Explicit lib path in install script

--- FastBCR_Docker/install_packages.R
+++ FastBCR_Docker/install_packages.R
@@ -1,30 +1,18 @@
 # install_packages.R
 
 # Set the library path explicitly
-# lib_path <- "/usr/local/lib/R/site-library"
+lib_path <- "/usr/local/lib/R/site-library"
 
 # Ensure the library directory exists
-# dir.create(lib_path, showWarnings = FALSE, recursive = TRUE)
+dir.create(lib_path, showWarnings = FALSE, recursive = TRUE)
 
 # Set the library path
-# .libPaths(c(lib_path, .libPaths()))
+.libPaths(c(lib_path, .libPaths()))
 
-
-
-
-# BiocManager
-install.packages("BiocManager")
-BiocManager::install(c("proj4", "msa", "ggtree", "ggmsa"), ask = FALSE)
-
-# install.packages("BiocManager", lib = lib_path)
-# BiocManager::install(c("proj4", "msa", "ggtree", "ggmsa"), lib = lib_path, ask = FALSE)
-
-# devtools
-install.packages(c('systemfonts', 'textshaping', 'ragg', 'pkgdown'))
-install.packages("devtools")
-# install.packages("devtools", lib = lib_path)
-
-# fastBCR
+# Install necessary packages
+install.packages("BiocManager", lib = lib_path)
+BiocManager::install(c("proj4", "msa", "ggtree", "ggmsa"), lib = lib_path, ask = FALSE)
+install.packages("devtools", lib = lib_path)
 devtools::install_github("ZhangLabTJU/fastBCR", ref = "v1.1.3")
 
 # Print library paths to verify
