6b60bf876753802184b78e89a0fff81e8e9dadb9
Cameron Hu <chu@profluent.bio>
2024-11-22 21:40:36 +0000

    gcs_input_list tsv format parsing logic

--- batch.py
+++ batch.py
@@ -44,114 +44,6 @@ def gcs_read(bucket_name, src_name):
         return None
 
 
-def split_fasta(input_path, output_dir, num_splits):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    total_size = 0
-    for r in Bio.SeqIO.parse(input_path, "fasta"):
-        total_size += len(r.seq)
-    split_size = int(total_size / num_splits)
-    split = -1
-    cursize = 0
-    out = None
-    for r in Bio.SeqIO.parse(input_path, "fasta"):
-        if out is None or cursize >= split_size:
-            split += 1
-            cursize = 0
-            out = open(os.path.join(output_dir, str(split)), "w")
-        out.write(">" + r.description + "\n" + str(r.seq) + "\n")
-        cursize += len(r.seq)
-    out.close()
-
-
-def filter_contigs(input_path, output_path, sample_name, min_contig_len, max_n_fract):
-    with gzip.open(input_path, "rt") as fin:
-        with open(output_path, "w") as fout:
-            for i, r in enumerate(Bio.SeqIO.parse(fin, "fasta")):
-                seq = str(r.seq).upper()
-                if len(seq) < min_contig_len:
-                    continue
-                elif 1.0 * seq.count("N") / len(seq) > max_n_fract:
-                    continue
-                fout.write(f">{sample_name}_{i} {r.description}\n{seq}\n")
-
-
-def run_command(cmd):
-    proc = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-    out, err = proc.communicate()
-    try:
-        retcode = proc.retcode
-    except AttributeError:
-        retcode = proc.returncode
-    if retcode != 0:
-        raise Exception(f"Subprocess error running: {cmd.split()}")
-
-
-def calculate_n50(contig_lengths):
-    total_length = sum(contig_lengths)
-    half_length = total_length / 2
-    running_sum = 0
-    for length in sorted(contig_lengths, reverse=True):
-        running_sum += length
-        if running_sum >= half_length:
-            return length
-
-
-def compute_summary_stats(contigs, proteins, output):
-    stats = {}
-
-    aalen = {"full": [], "partial": [], "all": []}
-    with open(proteins, "rt") as handle:
-        for _ in Bio.SeqIO.parse(handle, "fasta"):
-            info = dict([_.split("=") for _ in _.description.split()[-1].split(";")])
-            aalen["all"].append(len(_.seq))
-            if info["partial"] == "00":
-                aalen["full"].append(len(_.seq))
-            else:
-                aalen["partial"].append(len(_.seq))
-
-    stats["protein"] = {}
-    for type in ["all", "full", "partial"]:
-        stats["protein"][type] = {}
-        stats["protein"][type]["count"] = len(aalen[type])
-        stats["protein"][type]["min_len"] = (
-            min(aalen[type]) if len(aalen[type]) > 0 else None
-        )
-        stats["protein"][type]["max_len"] = (
-            max(aalen[type]) if len(aalen[type]) > 0 else None
-        )
-        stats["protein"][type]["avg_len"] = (
-            round(np.mean(aalen[type]), 2) if len(aalen[type]) > 0 else None
-        )
-        stats["protein"][type]["sum_len"] = sum(aalen[type])
-
-    with open(contigs, "rt") as handle:
-        contig_lengths = [len(_.seq) for _ in Bio.SeqIO.parse(handle, "fasta")]
-        stats["contig"] = {}
-        stats["contig"]["count"] = len(contig_lengths)
-        stats["contig"]["min_len"] = (
-            min(contig_lengths) if len(contig_lengths) > 0 else None
-        )
-        stats["contig"]["max_len"] = (
-            max(contig_lengths) if len(contig_lengths) > 0 else None
-        )
-        stats["contig"]["avg_len"] = (
-            round(np.mean(contig_lengths), 2) if len(contig_lengths) > 0 else None
-        )
-        stats["contig"]["sum_len"] = sum(contig_lengths)
-        stats["contig"]["n50"] = calculate_n50(contig_lengths)
-        stats["contig"]["cds_density"] = (
-            round(
-                100 * stats["protein"]["full"]["sum_len"] * 3 / sum(contig_lengths), 2
-            )
-            if len(contig_lengths) > 0
-            else None
-        )
-
-    with open(output, "w") as fout:
-        fout.write(json.dumps(stats, indent=2))
-
-
 class Pipeline:
     def __init__(self):
         self.parse_args()
@@ -159,17 +51,43 @@ class Pipeline:
         pprint(self.args)
 
     def fetch_inputs(self):
+        """
+        Fetches the run-to-files mapping from the batch input file and stores it in self.run_to_files.
+        It uses self.args['batch_input'] to locate the .tsv file and parses the data into a dictionary.
+        Only parses the number of inputs as calculated by batch_size
 
+        Populates:
+            self.run_to_files (dict): Dictionary mapping run IDs to their corresponding list of file paths.
+        """
         input_start = self.args["batch_size"] * self.args["batch_task_index"]
         input_stop = input_start + self.args["batch_size"]
 
-        if os.path.exists(self.args["batch_input"]):
-            input_list = [_.split() for _ in open(self.args["batch_input"])]
-        else:
-            bucket, path = self.args["batch_input"].split("/", 1)
-            input_list = [_.split("\t")[0] for _ in gcs_read(bucket, path).split("\n")]
+        # Initialize run_to_files dictionary
+        run_to_files = {}
 
-        self.input_list = input_list[input_start:input_stop]
+        # Check if the input file exists locally or in GCS
+        if os.path.exists(self.args["batch_input"]):
+            with open(self.args["batch_input"], "r") as f:
+                lines = f.readlines()
+        else:
+            # Parse GCS input file
+            bucket, path = self.args["batch_input"].split("/", 1)
+            tsv_content = gcs_read(bucket, path)
+            lines = tsv_content.split("\n")
+
+        # Exclude the header and fetch the relevant batch lines
+        lines = lines[1:]  # Exclude header
+        batch_lines = lines[input_start:input_stop]
+
+        # Populate the run_to_files dictionary
+        for line in batch_lines:
+            if line.strip():  # Skip empty lines
+                run_id, files_str = line.split("\t")
+                run_to_files[run_id] = eval(
+                    files_str
+                )  # Convert stringified list to a Python list
+
+        self.run_to_files = run_to_files
 
     def parse_args(self):
 
