cbd101447efdc164311d44fc4bd7859161c65895
Cameron Hu <chu@profluent.bio>
2024-11-22 00:39:31 +0000

    Updates

---
+++ batch_test.sh
@@ -0,0 +1,17 @@
+
+cd /home/cameronhu/lineage_tree
+
+# list gcs inputs
+gsutil ls gs://proevo-ab/lineages/fastbcr/input/runs/*.csv > gcs_input_list.txt
+
+# copy input file to gcs
+gsutil cp gcs_input_list.txt gs://proevo-ab/lineages/fastbcr/batch/gcs_input_list.txt
+
+# run test
+export BATCH_TASK_INDEX=0
+input=proevo-ab/lineages/fastbcr/batch/gcs_input_list.txt
+python batch.py --batch_size=1 --batch_input=$input
+
+
+
+
